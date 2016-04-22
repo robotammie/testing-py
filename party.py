85ca9ad8e1f3b31dca7b57bf434b5919b095e61f
Hackbright Student <info@hackbrightacademy.com>
2016-04-22 12:49:51 -0700

    added doctests

--- party.py
+++ party.py
@@ -9,13 +9,49 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+
+        Doctest below:
+
+        >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+        True
+
+        >>> is_mel('Judith Butler', 'judith@ubermelon.com')
+        False
+
+
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
-    """Given list of treats, return {most, least} common types."""
+    """Given list of treats, return {most, least} common types.
+
+    It should work as follows:
+        
+        >>> most_and_least_common_type([{'type': 'dessert'},
+        ...                             {'type': 'dessert'},
+        ...                             {'type': 'appetizer'},
+        ...                             {'type': 'dessert'},
+        ...                             {'type': 'appetizer'},
+        ...                             {'type': 'drink'}])
+        ('dessert', 'drink')
+
+        >>> most_and_least_common_type([{'type':'drink'},
+        ...                             {'type':'drink'}])
+        ('drink', 'drink')
+
+        >>> most_and_least_common_type([])
+        (None, None)
+
+        >>> most_and_least_common_type([{'type': 'dessert'},
+        ...                             {'type':'drink'},
+        ...                             {'type':'drink'},
+        ...                             {'type': 'dessert'}])
+        ('dessert', 'dessert')
+
+    """
 
     types = {}
 
--- tests.py
+++ tests.py
@@ -15,20 +15,25 @@ class PartyTests(unittest.TestCase):
         self.assertIn("I'm having a party", result.data)
 
     def test_no_rsvp_yet(self):
-        # FIXME: Add a test to show we haven't RSVP'd yet
-        print "FIXME"
+        result = self.client.get("/")
+        self.assertIn("Please RSVP", result.data)
+        self.assertNotIn("Party Details", result.data)
 
     def test_rsvp(self):
         result = self.client.post("/rsvp",
                                   data={'name': "Jane", 'email': "jane@jane.com"},
                                   follow_redirects=True)
-        # FIXME: check that once we log in we see party details--but not the form!
-        print "FIXME"
+        self.assertNotIn("Please RSVP", result.data)
+        self.assertIn("Party Details", result.data)
 
     def test_rsvp_mel(self):
         # FIXME: write a test that mel can't invite himself
-        pass
-        print "FIXME"
+
+        result = self.client.post("/rsvp",
+                                  data={'name': "Mel Melitpolski", 'email': "mel@ubermelon.com"},
+                                  follow_redirects=True)
+        self.assertIn("Please RSVP", result.data)
+        self.assertNotIn("Party Details", result.data)
 
 
 if __name__ == "__main__":
